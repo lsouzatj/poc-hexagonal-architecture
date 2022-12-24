8afe5d63d8e7aab67643f58b13dbaf1dbb62c13c
Francis Leandro de Souza <francis.leandro@oi.net.br>
2022-12-24 06:59:51 -0300

    feat: Criação do projeto.

---
+++ src/main/java/com/br/hexagonal/PocHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.br.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/hexagonal/PocHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
